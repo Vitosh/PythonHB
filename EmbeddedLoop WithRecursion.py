e5141b0bfd4af0af0be8a92a9818e38b6563e4b5
Vitosh Academy <vitosh.doynov@gmail.com>
2015-10-16 01:26:16 +0200

    Create EmbeddedLoop WithRecursion.py
    In stead of using 4 ugly embedded loops, we may use recursion.

---
+++ EmbeddedLoop WithRecursion.py
@@ -0,0 +1,17 @@
+k = 4
+c = [1,2,3,4,5,6]
+n = len(c)
+arr = [None]*k
+
+
+def embedded_loops(index):
+    if index >= k:
+        print(arr)
+    else:
+        for i in range(0, n):
+            arr[index] = i
+            embedded_loops(index+1)
+            
+
+        
+embedded_loops(0)
